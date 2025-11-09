b5281c03e0fba5caa637e82dd17c2cb2a48c2355
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-11-09 17:13:36 +0200

    New file and updates

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent(){
+    return (
+        <main>
+  <p style = {{fontSize: "40px", color:"green", border:"2px dotted white"}}>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+    )
+}
+
+export default MainContent;
