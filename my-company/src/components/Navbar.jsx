bdedbb633e202e79323b132446f8f6c1b5254772
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-11-12 20:33:52 +0200

    New updates

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav>
-            <ul>
+        <nav style= {{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+            <ul style= {{ listStyleType: "none", display: "flex", gap: "20px" }}>
                 <li><Link to="/Home">Home</Link></li>
                 <li><Link to="/About">About</Link></li>
                 <li><Link to="/Contact">Contact</Link></li>
