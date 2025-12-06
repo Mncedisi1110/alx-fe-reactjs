62186d36176de8de212dba0badbb285986cb2d1e
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-12-06 13:25:48 +0200

    New updates

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
   return (
-    <div class="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg" className="user-profile">
-      <img class="rounded-full w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p class="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div class="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4,max-w-xs md:p-8,max-w-sm" className="user-profile">
+      <img class="rounded-full sm:w-24,h24 md:w-36,h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
+      <h1 class="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
+      <p class="text-gray-600 md:text-base sm:text-sm">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
