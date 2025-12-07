368b260e20a49c2290d6955dd24a3f55157216ff
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-12-07 22:31:19 +0200

    New updates.

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useEffect , useState } from "react";
+import router from "react-router-dom";
 
 const HomePage = () => {
     const [recipes, setRecipes] = useState([]);
@@ -23,7 +24,9 @@ const HomePage = () => {
         return <div>Loading...</div>;
     }
     return (
+
         <div className="container mx-auto p-4 bg-gray-400">
+            <router.Link to="./components/RecipeDetail" className="btn btn-primary mb-4" />
             <h1>Recipe Sharing Platform</h1>
             <ul className =" grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-2 p-6 rounded shadow-md hover:shadow-gray-300 ,rounded-10 text-gray-400  ">
                 {recipes.map((recipe) => (
