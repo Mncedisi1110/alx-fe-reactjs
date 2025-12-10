1ba2c6464dda77a16c97fc2f0da2719e88e88c3d
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-12-10 15:48:34 +0200

    New updates.

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -4,10 +4,26 @@ function RegistrationForm() {
        const [username, setUsername] = useState("")
        const [email, setEmail] = useState("")
        const [password, setPassword] = useState("")
-       
+       const [errors, setErrors] = useState({})
+
+       function validateForm() {
+        const newErrors = {};
+
+        if (!username === "") {
+            newErrors.username = "Username is required.";
+        }
+        if (!email.includes("@")) {
+            newErrors.email = "Invalid email address.";
+        }
+        if (!password || password.length < 6) {
+            newErrors.password = "Password must be at least 6 characters long.";
+        }
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+       }
 
 
-    
+
      const handleSubmit= (e) =>{
         e,preventDefault();
      }
@@ -17,12 +33,14 @@ function RegistrationForm() {
 
             <label>
                 Enter Your Username:
-                <input 
+                <input
                 type ="text"
                 value={username}
+                errors={validateForm.username}
                 onChange = {setUsername((e)=>{
                     e.target.value
                 })}
+
                 required
                 />
             </label>
@@ -32,6 +50,7 @@ function RegistrationForm() {
                 <input 
                 type ="email"
                 value={email}
+                errors={validateForm.email}
                 onChange = {setEmail((e)=>{
                     e.target.value
                 })}
@@ -45,6 +64,7 @@ function RegistrationForm() {
                 <input 
                 type = "password"
                 value={password}
+                errors={validateForm.password}
                 onChange = {setPassword((e)=>{
                     e.target.value
                 })}
