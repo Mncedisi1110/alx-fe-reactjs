5a5eb712ce7d752762065e8604109082ee130bfd
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-12-14 18:03:39 +0200

    New updates.

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -8,16 +8,23 @@ const fetchPosts = async() => {
 
 
 function PostsComponent() {
-    const { data, isError, isLoading } = useQuery('posts', fetchPosts);
+    const { data,error, isError, isLoading } = useQuery('posts', fetchPosts);
 
-    if (isLoading) return <div>Loading...</div>;
-    if (isError) return <div>Error fetching data</div>;
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    if (isError) {
+        return <div>Error: {error.message}</div>;
+    }
     return (
         <div>
             <h1>Posts</h1>
             <ul>
                 {data.map(post => (
-                    <li key={post.id}>{post.title}</li>
+                    <li key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                    </li>
                 ))}
             </ul>
         </div>
